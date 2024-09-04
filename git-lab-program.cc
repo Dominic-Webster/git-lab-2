d848b4c50be4cb2826a7a5c70cc33386d71744f1
Dominic Webster <dominicbweiss@gmail.com>
2024-09-04 13:57:02 -0400

    message

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Dominic Webster
+ *        Date: 9-4-2024
+ * Description: A touching description that moves readers to tears
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
